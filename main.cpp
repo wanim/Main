5cbf3eae4b71af3453ac7dddae9a65e95070b615
wanim <wanim@users.noreply.github.com>
2015-04-02 12:35:28 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ int  i __attribute__((aligned(8)));
+ short c __attribute__((aligned(8))) = 7;
+ int j __attribute__((aligned(8))) = 9;
+ int k __attribute__((aligned(8))) = 0;
+
+
+
+int main()
+{
+ *(((int*)&j) - 1) = 7;
+ printf("%d,%d,%d,%d\n", i, c, j, k);
+ printf("%p,%p,%p, %p", &i, &c, &j, &k);
+ return 0;
+}
