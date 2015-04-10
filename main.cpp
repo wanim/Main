f14460f6032987f7eb57b44f9c7c26f06614f153
wanim <wanim@users.noreply.github.com>
2015-04-10 16:58:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-void *memcpy(void *dest, const void *src, size_t n)
-{
-    register char* d1 = (char*)dest;
-    register char* s1 = (char*)src;
-    register size_t len = n;
 
-    while(len--)
-    {
-        *d1++ = *s1++;
-    }
-    return dest;
-}
