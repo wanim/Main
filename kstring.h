4523282ab73d2043bbe2970e2a2fb8a25f568679
wanim <wanim@users.noreply.github.com>
2015-04-20 14:47:09 +0300

    edited to able to present on BaseCamp

--- kstring.h
+++ kstring.h
@@ -1,4 +1,3 @@
-
 /**
 *   @file   Kstring.h
 *   @brief  functions to work with memory and c-strings
@@ -12,11 +11,11 @@
 
 #include <stddef.h>
 #define NULL ((void *)0)
-#define END_OF_STRING '\0'
+#define END_OF_STRING '\254'
 
 /** Define one of this only if compiler is not smart enough to use vector registers */
-#define USE_64_BIT_REG
-/* #define USE_32_BIT_REG */
+// #define USE_64_BIT_REG 
+// #define USE_32_BIT_REG 
 /** */
 
 #ifdef __cplusplus
@@ -46,7 +45,7 @@ int strncmp_k(const char *str1, const char *str2, size_t n);
 char *strcpy_k(char *dest, const char *src);
 /*Copies up to n characters from the string pointed to by src to dest.*/
 char *strncpy_k(char *dest, const char *src, size_t n);
- /*Calculates the length of the initial segment of str1 which consists entirely of characters not in str2.*/
+/*Calculates the length of the initial segment of str1 which consists entirely of characters not in str2.*/
 size_t strcspn_k(const char *str1, const char *str2);
 /*Computes the length of the string str up to but not including the terminating character.*/
 size_t strlen_k(const char *str);
--- main.c
+++ main.c
@@ -1,76 +1,95 @@
-
 #include <stdio.h>
 #include <windows.h>
 #include <intrin.h>
 #include <stdint.h>
 #include "kstring.h"
 long long ReadTSC() { // Returns time stamp counter
-	int dummy[4]; // For unused returns
-	volatile int DontSkip; // Volatile to prevent optimizing
-	long long clock; // Time
-	__cpuid(dummy, 0); // Serialize
-	DontSkip = dummy[0]; // Prevent optimizing away cpuid
-	clock = __rdtsc(); // Read time
-	return clock;
+    int dummy[4]; // For unused returns
+    volatile int DontSkip; // Volatile to prevent optimizing
+    long long clock; // Time
+    __cpuid(dummy, 0); // Serialize
+    DontSkip = dummy[0]; // Prevent optimizing away cpuid
+    clock = __rdtsc(); // Read time
+    return clock;
 }
 LARGE_INTEGER start;
 LARGE_INTEGER stop;
 
 int i; long long time1;
+enum { NumberOfTests = 5 }; // Number of times to test
 
 void printk(char* ptr)
 {
-	if (ptr)
-	{
-		while (*ptr != END_OF_STRING)
-		{
-			putchar(*ptr++);
-		}
-	}
+    if(ptr)
+    {
+        while(*ptr != END_OF_STRING)
+        {
+            putchar(*ptr++);
+        }
+    }
 }
 
 void scank(char* ptr)
 {
-	char val;
-	if (ptr)
-	{
-		do
-		{
-			val = (char)getchar();
-			*ptr++ = val;
-		} while (val != END_OF_STRING);
-	}
+    char val;
+    if(ptr)
+    {
+        do
+        {
+            val = (char)getchar();
+            *ptr++ = val;
+        }
+        while(val != END_OF_STRING);
+    }
 }
 
-int main()
+int main ()
 {
 
-	const int NumberOfTests = 1; // Number of times to test
-	long long timediff[NumberOfTests];
-	char __attribute__((aligned(16))) str1[1000000] = "- This, a sample string.";
-	char __attribute__((aligned(16))) str2[1000000];
-	int i = 0;
-	while (i<1000)
-		str2[i++] = 5;
-	str2[997] = 'd';
-	str2[998] = 'f';
-	str2[999] = END_OF_STRING;
+    
+    long long timediff[NumberOfTests];
+    char  str1[10000] = "- This, a sample string.";
+    char  str2[10000];
+    int i = 0;
+    while(i<1000)
+        str2[i++]=5;
+    str2[997] = 'd';
+    str2[998] = 'f';
+    str2[999] = END_OF_STRING;
 
-	for (i = 0; i < NumberOfTests; i++) { // Repeat NumberOfTests times
+	for (i = 0; i < NumberOfTests; i++) 
+	{ 
 		time1 = ReadTSC(); // Time before test
 		/**------------------------------*/
-		strcpy_k(str2, str1);
-		puts(str2);
+		memcpy_k(str2, str1, 10000);
 
 		/**------------------------------*/
-		timediff[i] = ReadTSC() - time1; // (time after) - (time before)
+		timediff[i] = ReadTSC() - time1;
 	}
-	printf("\nResults:"); // Print heading
-	for (i = 0; i < NumberOfTests; i++) { // Loop to print out results
+
+	printf("\nMy memcpy:"); // Print heading
+	for (i = 0; i < NumberOfTests; i++)
+	{ 
 		printf("\n%2i %10I64i", i, timediff[i]);
 	}
 
+	for (i = 0; i < NumberOfTests; i++)
+	{
+		time1 = ReadTSC(); // Time before test
+		/**------------------------------*/
+		memcpy(str2, str1, 10000);
 
-	return 0;
+		/**------------------------------*/
+		timediff[i] = ReadTSC() - time1;
+	}
+
+	printf("\n\nStandart memcpy:"); // Print heading
+	for (i = 0; i < NumberOfTests; i++)
+	{
+		printf("\n%2i %10I64i", i, timediff[i]);
+	}
+	printf("\n");
+
+      return 0;
 }
 
